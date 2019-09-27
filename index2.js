ae188c9c7c4a057c4b3abc3a134784efddc4dd96
HoangHung <hoanghunght2304@gmail.com>
2019-09-28 01:41:40 +0700

    Edit index2

--- index2.js
+++ index2.js
@@ -1,2 +1,6 @@
-setTimeout( () => console.log("Hello World"));
-console.log('haha');
+function tossCoin(){
+    let value = Math.random();
+    let result = (value > 0.5) ? 'Mặt sấp' : 'Mặt ngửa';
+    console.log(value);
+    console.log(result);
+}
